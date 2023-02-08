549a97619a8e449c883c0cc0cd56b7b2d2c1edd3
andzzz-hex <almeidasantos.andre@gmail.com>
2023-02-08 18:05:38 +0000

    nav working!

--- src/App.js
+++ src/App.js
@@ -1,50 +1,24 @@
 import './styles.css';
-// router
-import {createBrowserRouter, RouterProvider, } from "react-router-dom";
-import { Outlet } from "react-router-dom";
-import Root from "./routes/root";
-import ErrorPage from "./error-page";
-import FindPatient from "./routes/findpatient";
+// router .. import route.js for each screen! 
+import { Routes, Route} from 'react-router-dom';
+import Dashboard from "./routes/dashboard";
+import Findpatient from "./routes/findpatient";
 // material ui
-import Button from '@mui/material/Button';
 import 'material-symbols';
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import Header from './components/header';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "findpatient",
-    element: <FindPatient />
-  }
-]);
+import ErrorPage from "./error-page";
 
+// app = all routes (pages) to cycle through 
 function App() {
   return (
-    <body>
-        <RouterProvider router={router} />
-        <div className='conteudo'>
-        <div className="topo">
-          <Header />
-          </div>
-        <hr></hr>
- 
-        <div className="maindiv"><h1>second block</h1></div>
-        <Grid container sx={{ color: "text.primary" }} >
-        <Grid item xs={20}>
-      </Grid>
-    </Grid>
-        
-        </div>
-    </body>
-  
-
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='findpatient' element={<Findpatient />} />
+      </Routes>
   );
 }
 
 export default App;
+
+
+
